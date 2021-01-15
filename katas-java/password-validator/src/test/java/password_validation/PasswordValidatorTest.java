a66648e0bb55cabf0e040eade95254d689cae8f4
Miriam Benito <miriam.benito@clarity.ai>
2021-01-15 11:06:11 +0100

    password-validator

--- katas-java/password-validator/src/test/java/password_validation/PasswordValidatorTest.java
+++ katas-java/password-validator/src/test/java/password_validation/PasswordValidatorTest.java
@@ -1,13 +1,66 @@
 package password_validation;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class PasswordValidatorTest {
 
-  @Test
-  public void fix_me_and_rename() {
-    assertFalse(true);
+  public Set<String> checkPassword(String password) {
+    var response = new HashSet<String>();
+
+    if (password.length() <= 8) {
+      response.add("moreThan8");
+    }
+    if (!password.contains("_")) {
+      response.add("hasUnderscore");
+    }
+    if (!password.matches(".*\\d.*")) {
+      response.add("hasNumber");
+    }
+    if (!password.matches(".*[A-Z].*")) {
+      response.add("hasCapitalLetter");
+    }
+    if (!password.matches(".*[a-z].*")) {
+      response.add("hasLowerCase");
+    }
+    return response;
   }
+
+  @Test
+  public void check_length_more_than_8() {
+    assertTrue(checkPassword("hi").contains("moreThan8"));
+  }
+
+  @Test
+  public void check_has_underscore() {
+    assertTrue(checkPassword("juanymiriam").contains("hasUnderscore"));
+  }
+
+  @Test
+  public void check_has_number() {
+    assertTrue(checkPassword("hi").contains("hasNumber"));
+  }
+
+  @Test
+  public void check_has_capital_letter() {
+    assertTrue(checkPassword("hi").contains("hasCapitalLetter"));
+  }
+
+  @Test
+  public void check_has_lower_case() {
+    assertTrue(checkPassword("57U").contains("hasLowerCase"));
+  }
+
 }
+
+// - Try with a password with lest than 9 characters
+// - Try with a password without an underscore
+// - Try with a password without a number
+// - Try with a password without a capital letter
+// - Try with a password without lowercase
